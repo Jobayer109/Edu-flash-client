99ef40b43089c0200d846aa84e7640705e71c331
Nightowlam <nightowl3.00am@gmail.com>
2022-10-25 22:04:12 +0600

    navbar included

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,38 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/Logo.png";
 
 const Header = () => {
   return (
-    <div className="navbar flex items-center justify-between bg-base-100 px-36">
+    <div className="navbar flex items-center justify-between px-36 mt-0">
       <div className="">
-        <Link className="btn btn-ghost normal-case text-xl">Edu Flash</Link>
+        <Link>
+          <img className="h-16" src={logo} alt="" />
+        </Link>
       </div>
 
-      <div>
-        <Link>Home</Link>
+      <div className="bg-gray-400 text-white p-1 rounded-lg">
+        <Link className="ml-8 motion-safe:hover:scale-110" to="/home">
+          Home
+        </Link>
+        <Link className="ml-8 motion-safe:hover:scale-110" to="/courses">
+          Courses
+        </Link>
+        <Link className="ml-8 motion-safe:hover:scale-110" to="/profile">
+          Profile
+        </Link>
+        <Link className="ml-8 motion-safe:hover:scale-110" to="/blogs">
+          Blogs
+        </Link>
+        <Link className="ml-8 motion-safe:hover:scale-110 mr-8" to="/faq">
+          FAQ
+        </Link>
       </div>
       <div className="flex-none gap-2">
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
+            <div className="w-12 rounded-full border-2 border-gray-400 ">
               <img src="https://placeimg.com/80/80/people" />
             </div>
           </label>
           <ul
             tabIndex={0}
-            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            className="mt-3 p-6 shadow menu menu-compact dropdown-content bg-black rounded-box w-52"
           >
             <li>
-              <a className="justify-between">
+              <Link
+                to="/profile"
+                className="justify-center font-bold border-b-2 text-white ransform hover:scale-110 motion-reduce:transform-none "
+              >
                 Profile
-                <span className="badge">New</span>
-              </a>
+              </Link>
             </li>
             <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
+              <button className="btn btn-error btn-outline my-3">Sign out</button>
             </li>
           </ul>
         </div>
