d40b2161eaa7d9bb3a76e8186f86548ccc096310
Nightowlam <nightowl3.00am@gmail.com>
2022-10-25 20:52:13 +0600

    favicon and name set

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home";
 
 export const router = createBrowserRouter([{
     path: '/',
@@ -8,7 +9,8 @@ export const router = createBrowserRouter([{
     errorElement: <ErrorPage></ErrorPage>,
     children: [
         {
-            
+            path: 'home',
+            element: <Home></Home>
         }
     ]
 }]) 
